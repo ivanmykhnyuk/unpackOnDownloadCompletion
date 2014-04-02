695088bef1a799828ad51817371d2185126d8fce
imykhnyuk <ivanmykhnyuk@gmail.com>
2014-04-02 10:42:15 -0700

    dkdfkdfk

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class Main {
 
+    private static final String INPUT_ZIP_FILE = "D:\\DeskPins130.zip";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        unZipIt(new File(INPUT_ZIP_FILE));
+    }
+
+    public static void unZipIt(File zipFile) {
+
+        byte[] buffer = new byte[1024];
+
+        try {
+            String outputFolder = zipFile.getParent();
+            //create output directory is not exists
+            File folder = new File(outputFolder);
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+
+            //get the zip file content
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+            //get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+
+            while (ze != null) {
+                String fileName = ze.getName();
+                File newFile = new File(outputFolder + File.separator + fileName);
+
+                //create all non exists folders
+                //else you will hit FileNotFoundException for compressed folder
+                new File(newFile.getParent()).mkdirs();
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();
+                ze = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+
+            //remove file
+            zipFile.delete();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
