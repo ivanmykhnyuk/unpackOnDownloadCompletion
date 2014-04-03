a9177abd7f96757e1ddcb2d53f8c083ea87ac25e
Ivan Mikhnyuk <i.mikhnyuk@samsung.com>
2014-04-03 10:41:54 +0300

    [Type] [Issue No] [Module] [Description] [Implementation]dsfs

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,76 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import name.pachler.nio.file.*;
+
+import java.io.*;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Main {
+    private static Path watchedDir;
 
-    private static final String INPUT_ZIP_FILE = "D:\\DeskPins130.zip";
+    private static boolean archiveFileDropped = false;
 
-    public static void main(String[] args) {
-        unZipIt(new File(INPUT_ZIP_FILE));
+
+    public static void main(String[] args) throws IOException {
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+
+        watchedDir = Paths.get("D://temp");
+        final WatchKey key = watchedDir.register(watchService, StandardWatchEventKind.ENTRY_CREATE);
+
+
+        Thread pollingThread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    List<name.pachler.nio.file.WatchEvent<?>> list = key.pollEvents();
+                    for (WatchEvent watchEvent : list) {
+                        archiveFileDropped = true;
+                        System.out.print("ololo, event....");
+
+                    }
+                }
+            }
+        };
+
+
+        Thread unpackingThread = new Thread() {
+
+            @Override
+            public void run() {
+                ArchiveFiles filter = new ArchiveFiles(new String[] {"zip"});
+
+                while (true) {
+                    if (archiveFileDropped) {
+                        File dkdfk = new File("D://temp");
+                        File [] list = dkdfk.listFiles(filter);
+                        for (File l : list) {
+                            unZipIt(l);
+                        }
+                        archiveFileDropped = false;
+
+                    } else {
+                        try {
+                            sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+
+
+        unpackingThread.start();
+        pollingThread.start();
+        try {
+            unpackingThread.join();
+            pollingThread.join();
+        } catch (InterruptedException e) {
+
+        }
+
+
+//        unZipIt(new File(INPUT_ZIP_FILE));
     }
 
     public static void unZipIt(File zipFile) {
@@ -19,7 +78,8 @@ public class Main {
         byte[] buffer = new byte[1024];
 
         try {
-            String outputFolder = zipFile.getParent();
+//            String outputFolder = zipFile.getParent();
+            String outputFolder = "D://temp";
             //create output directory is not exists
             File folder = new File(outputFolder);
             if (!folder.exists()) {
@@ -57,6 +117,28 @@ public class Main {
             zipFile.delete();
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+
+    // inner class, generic extension filter
+    public static class ArchiveFiles implements FilenameFilter {
+        private String[] supportedArchiveFormats = {"zip"};
+
+        public ArchiveFiles(String [] ext) {
+            this.supportedArchiveFormats = ext;
+        }
+
+        public boolean accept(File dir, String name) {
+            boolean acceptableFile = false;
+            for (String fileFormat : supportedArchiveFormats) {
+                if (name.endsWith(fileFormat)) {
+                    acceptableFile = true;
+                    break;
+                }
+            }
+
+            return acceptableFile;
         }
     }
 }
